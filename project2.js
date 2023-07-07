c5e1e14450ca28926876c1c20e8fa98142e620c9
AVIS10060 <138568046+AVIS10060@users.noreply.github.com>
2023-07-07 23:39:17 +0530

    Update project2.js

--- project2.js
+++ project2.js
@@ -1,34 +1,29 @@
-const sliderContainer = document.querySelector('.slider-container');
-const slideLeft =  document.querySelector('.right-slide');
-const slideRight = document.querySelector('.right-slide');
-const upButton = document.querySelector('.up-button');
-const downButton = document.querySelector('.down-button');
-const slidesLength =  slideRight.querySelectorAll('div').length
+const sliderContainer = document.querySelector(".slider-container");
+const slideRight = document.querySelector(".right-side");
+const slideLeft = document.querySelector(".left-side");
+const upButton = document.querySelector(".up-button");
+const downButton = document.querySelector(".down-button");
+const slidesLength = slideRight.querySelectorAll("div").length;
 
-let activeSlideIndex = 0 ;
+let activeSlideIndex = 0;
+slideLeft.style.top = `-${(slidesLength - 1)*100}vh`;
+upButton.addEventListener('click', ()=>changeSlide('up'))
+downButton.addEventListener('click', ()=>changeSlide('down'))
 
-slideLeft.style.top=` -${(slidesLength-1)*100}vh`
-
-upButton.addEventListener(`click`,()=> changeSlide('up'))
-downButton.addEventListener(`click`,()=>changeSlide('down'))
-
-const changeSlide = (direction) =>{
-    const slidesHeight = sliderContainer.clientHeight
-    if(direction === 'up'){
+const changeSlide = (direction)=>{
+    const sliderHeight = sliderContainer.clientHeight
+    if(direction ==='up') {
         activeSlideIndex++
-        if(activeSlideIndex > slidesLength - 1){
-            activeSlideIndex=0
-
+        if (activeSlideIndex > slidesLength - 1 ){
+            activeSlideIndex = 0
         }
-    }
-    else if(direction === 'down'){
+    }else if(direction ==='down') {
         activeSlideIndex--
-        if(activeSlideIndex < 0){
-            activeSlideIndex = slidesLength - 1
-
+        if (activeSlideIndex < 0 ){
+            activeSlideIndex = slidesLength-1
         }
     }
 
-slideLeft.style.transform = `translateY(${activeSlideIndex *  slidesHeight  }px)`
-slideRight.style.transform = `translateY(-${activeSlideIndex *  slidesHeight  }px)`
+    slideRight.style.transform = `translateY(-${activeSlideIndex * sliderHeight}px)`
+    slideLeft.style.transform = `translateY(${activeSlideIndex * sliderHeight}px)`;
 }
